92d5fbb421e3ab7ec668aa473127d34889ad07a9
Felix M. <me@felix.hamburg>
2025-07-30 20:45:08 +0200

    ✅ :: Add tests for `reorderSubsetInPlace()`

---
+++ server/src/utils/reorderSubsetInPlace.test.ts
@@ -0,0 +1,63 @@
+import { expect, describe, it } from 'vitest';
+import { reorderSubsetInPlace } from './reorderSubsetInPlace';
+
+describe(`test method "reorderSubsetInPlace()"`, () => {
+  it('should handle an empty `array` parameter.', () => {
+    // Given
+    const array = [];
+    const newSubsetOrder = [];
+
+    // When
+    const result = reorderSubsetInPlace(array, newSubsetOrder);
+
+    // Then
+    expect(result).toStrictEqual([]);
+  });
+
+  it('should handle an empty `newSubsetOrder` parameter.', () => {
+    // Given
+    const array = ['a', 'b', 'c', 'd', 'e', 'f'];
+    const newSubsetOrder = [];
+
+    // When
+    const result = reorderSubsetInPlace(array, newSubsetOrder);
+
+    // Then
+    expect(result).toStrictEqual(['a', 'b', 'c', 'd', 'e', 'f']);
+  });
+
+  it('should throw an error due to the `newSubsetOrder` parameter containing an invalid element.', () => {
+    // Given
+    const array = ['a', 'b', 'c', 'd', 'e', 'f'];
+    const newSubsetOrder = ['a', 'b', 'g'];
+
+    // When
+    expect(() => reorderSubsetInPlace(array, newSubsetOrder))
+      // Then
+      .toThrowError();
+  });
+
+  it('should not reorder elements due to a single item in the `newSubsetOrder` parameter.', () => {
+    // Given
+    const array = ['a', 'b', 'c', 'd', 'e', 'f'];
+    const newSubsetOrder = ['c'];
+
+    // When
+    const result = reorderSubsetInPlace(array, newSubsetOrder);
+
+    // Then
+    expect(result).toStrictEqual(['a', 'b', 'c', 'd', 'e', 'f']);
+  });
+
+  it('should reorder elements according to the `newSubsetOrder` parameter.', () => {
+    // Given
+    const array = ['a', 'b', 'c', 'd', 'e', 'f'];
+    const newSubsetOrder = ['e', 'a', 'c'];
+
+    // When
+    const result = reorderSubsetInPlace(array, newSubsetOrder);
+
+    // Then
+    expect(result).toStrictEqual(['e', 'b', 'a', 'd', 'c', 'f']);
+  });
+});
